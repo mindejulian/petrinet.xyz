0432fd4caae8d5887f905f6fbe88fbbf1fd164fd
Julian Minde <julian.minde@webstep.no>
2019-03-06 10:25:43 +0100

    Added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import PetriNetView from './components/PetriNetView';
 import ModelPicker from './components/ModelPicker';
+import ReactGA from 'react-ga';
 
 class App extends Component<{}, {}> {
+  componentDidMount = () => {
+    ReactGA.initialize('UA-135652424-1');
+    ReactGA.pageview('/');
+  }
+
   render() {
     return (
       <div className="App">
